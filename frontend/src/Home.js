956783583d4f825583f8512a5312b6f574f61d8d
guragura <87663920+ReZa981@users.noreply.github.com>
2023-04-15 22:18:52 +0700

    fix:kohub name

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -3,8 +3,14 @@ import Navbar from './Navbar';
 
 function Home() {
   return (
-    <div className="Home">
+    <div className="home">
       <Navbar />
+      <div className="home-display">
+        <div className="homeinfo">
+          <p className="kohubname">KOHUB</p>
+        </div>
+        {/* <img src={`${process.env.PUBLIC_URL}/kohub.png`} className="duckfloat" /> */}
+      </div>
    </div>
   );
 }
