c7175f11d664e6f63329e79be8ca71a035c9567a
guragura <87663920+ReZa981@users.noreply.github.com>
2023-04-19 16:25:12 +0700

    fix: more info button

---
+++ frontend/src/permanent/Moreinfo.js
@@ -0,0 +1,13 @@
+import DefaultLayout from '../layout/DefaultLayout';
+
+function Moreinfo() {
+  return (
+    <DefaultLayout>
+        <div>
+            Placeholder (Put about section here)
+        </div>
+    </DefaultLayout>
+  );
+}
+
+export default Moreinfo;
