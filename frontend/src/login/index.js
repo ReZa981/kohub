1f3f9639156ead4600089cdc63d3203e63b34ad3
guragura <87663920+ReZa981@users.noreply.github.com>
2023-04-23 17:39:36 +0700

    feat: login

--- frontend/src/login/index.js
+++ frontend/src/login/index.js
@@ -1,17 +1,50 @@
+import { useState } from 'react';
 import DefaultLayout from '../layout/DefaultLayout';
 import './index.css';
 
-function admin() {
-    return 
+const role = {
+	Admin: Symbol("admin"),
+	User: Symbol("user"),
 }
-function user() {
 
-}
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [userRole, setUserRole] = useState(role.User);
+
   return (
     <DefaultLayout>
         <div className="login">
-          
+          <div className='loginrole'>
+            <a
+              className={userRole === role.User ? 'current' : ''} href="#" onClick={() => setUserRole(role.User)}
+            >
+              User
+            </a>{' '}
+            <a
+              className={userRole === role.Admin ? 'current' : ''} href="#" onClick={() => setUserRole(role.Admin)}
+            >
+              Admin
+            </a>
+          </div>
+          <div class="loginbox">
+            <label htmlFor="txtUsername">Username</label>
+              <input
+                type="text"
+                id="txtUsername"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            <label htmlFor="txtPassword">Password</label>
+              <input
+                type="password"
+                id="txtPassword"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+             <button type="submit" id="btnLogin" > Log in</button>
+             <p className='tosignup'>Don’t have any account ? <a href="signup" className="linksignup">Sign up</a></p>
+          </div>
         </div>
     </DefaultLayout>
   );
